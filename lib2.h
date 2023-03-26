a968f8625995f5063ae98f1b2c79757a12d8bbfc
tamco61 <42232243+tamco61@users.noreply.github.com>
2023-03-26 11:52:57 +0300

    Update lib2.h

--- lib2.h
+++ lib2.h
@@ -2,8 +2,8 @@
 #define LIB2_H
 #include <math.h>
 
-float SinIntegral(float a, float b, float e);
+extern "C" float SinIntegral(float a, float b, float e);
 
-int PrimeCount(int A, int B);
+extern "C" int PrimeCount(int A, int B);
 
 #endif
